57710b258608fee88fbfa8203f6c7b324a443418
gaurav5xy <gauravkumar81464@gmail.com>
2024-11-06 13:01:59 +0530

    newVideo

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
       <div className=" w-screen h-screen fixed top-0 left-0 z-0">
         {/* Background video */}
         <video className=" w-screen h-screen object-cover" width="1280" height="720" playsInline autoPlay loop muted preload="none">
-          <source src="/video/mainvideo.mp4" type="video/mp4" />
+          <source src="/video/main.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
